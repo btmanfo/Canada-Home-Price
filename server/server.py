a33a91e1137ff4f2e4cda2935209a2a379e56af6
Brice <bricetangmouo@gmail.com>
2025-11-15 02:11:14 -0500

    used a neural network for better prediction

--- server/server.py
+++ server/server.py
@@ -1,32 +1,40 @@
 from flask import Flask, request, jsonify
-app = Flask(__name__)
+import os
+import sys
+
+# Ajouter le dossier model au path
+sys.path.append(os.path.join(os.path.dirname(__file__), "../model"))
 import utils
 
-@app.route('/get_name_element', methods = ['GET'])
+app = Flask(__name__)
+
+
+@app.route('/get_name_element', methods=['GET'])
 def get_all_columns():
     columns_name = utils.get_all_columns_data()
-    reponse = jsonify({
-        'province':columns_name['province'],
+    response = jsonify({
+        'province': columns_name['province'],
         'city': columns_name['city'],
         'home_type': columns_name['type'],
         'smoking_permission': columns_name['is_smoking']
     })
-    reponse.headers.add('Access-Control-Allow-Origin', '*')
-    return reponse
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
-@app.route('/predict_home_price', methods = ['POST'])
+
+@app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
     province = request.form['province']
-    city= request.form['city']
+    city = request.form['city']
     home_type = request.form['home_type']
     smoking_permission = request.form['smoking_permission']
-    beds = int(request.form['beds'])
+    beds = float(request.form['beds'])
     baths = float(request.form['baths'])
     sq_feet = float(request.form['sq_feet'])
-    cats_allowed = int(request.form['cats'])
-    dogs_allowed = int(request.form['dogs'])
-    #LOL I HAVE DUPLICATE FKKKKK
+    cats = int(request.form['cats'])
+    dogs = int(request.form['dogs'])
     nbr_beds = float(request.form['nb_beds'])
+
     dummies_transformed = {
         "province": province,
         "city": city,
@@ -37,13 +45,14 @@ def predict_home_price():
         "beds": beds,
         "baths": baths,
         "sq_feet": sq_feet,
-        "cats": cats_allowed,
-        "dogs": dogs_allowed,
-        'nbr_beds': nbr_beds
+        "cats": cats,
+        "dogs": dogs,
+        "nbr_beds": nbr_beds
     }
-    response = jsonify({
-        'estimated_price': utils.get_estimation_price(dummies_transformed,quantified_variables)
-    })
+
+    estimated_price = utils.get_estimation_price(dummies_transformed, quantified_variables)
+
+    response = jsonify({'estimated_price': estimated_price})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
--- server/utils.py
+++ server/utils.py
@@ -1,46 +1,45 @@
-import json
-import pickle
-import numpy as np
 import os
-__all_columns_unclassified = None
-__model = None
+import json
+import numpy as np
+from tensorflow.keras.models import load_model
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-def get_all_columns_data() -> json:
-    all_data_columns = None
-    global __all_columns_unclassified
-    global __model
-    with open(os.path.join(BASE_DIR, "columns_without_classification.json")) as f:
-        __all_columns_unclassified = json.load(f)
-    with open(os.path.join(BASE_DIR, "columns.json"), 'r') as f:
-        all_data_columns = json.load(f)
-    with open(os.path.join(BASE_DIR, "price_prediction_model.pickle"), 'rb') as f:
-        __model = pickle.load(f)
-    return all_data_columns
+MODEL_PATH = os.path.join(BASE_DIR, "price_prediction_model.keras")
+COLUMNS_PATH = os.path.join(BASE_DIR, "columns_without_classification.json")
+ALL_COLUMNS_JSON = os.path.join(BASE_DIR, "columns.json")
 
-def get_estimation_price(string_defination:dict, quantity_defination:dict):
-    #get_all_columns_data()
-    province_index = __all_columns_unclassified.index(string_defination['province'])
-    city_index = __all_columns_unclassified.index(string_defination['city'])
-    home_type = __all_columns_unclassified.index(string_defination['home_type'])
-    smoking_permisstion = __all_columns_unclassified.index(string_defination['is_smoking'])
+__model = load_model(MODEL_PATH)
 
-    x= np.zeros(len(__all_columns_unclassified)+7)
-    x[province_index] =1
-    x[city_index] =1
-    x[home_type] =1
-    x[smoking_permisstion] =1
-    x[0] = quantity_defination['beds']
-    x[1] = quantity_defination['baths']
-    x[2] = quantity_defination['sq_feet']
-    x[3] = quantity_defination['cats']
-    x[4] = quantity_defination['dogs']
-    x[5] = quantity_defination['nbr_beds']
-
-    return round(__model.predict([x])[0], 2)
+with open(COLUMNS_PATH, 'r') as f:
+    __all_columns_unclassified = json.load(f)
 
 
-if __name__ == '__main__':
-    get_all_columns_data()
-    dummies_transformed = {'province':'alberta', 'city':'airdrie', 'home_type':'townhouse', 'is_smoking':'non-smoking'}
-    quantified_variables = {'beds':2, 'baths':2.5, 'sq_feet':1403.0,'cats':1, 'dogs':1,'nbr_beds':2.0}
-    print(get_estimation_price(dummies_transformed, quantified_variables))
+def get_all_columns_data():
+    """Retourne les colonnes catégorielles disponibles"""
+    with open(ALL_COLUMNS_JSON, 'r') as f:
+        return json.load(f)
+
+
+def get_estimation_price(string_definition: dict, quantity_definition: dict) -> float:
+    """Retourne la prédiction du prix en utilisant le modèle Keras"""
+    x = np.zeros(len(__all_columns_unclassified) + 7)
+
+    x[__all_columns_unclassified.index(string_definition['province'])] = 1
+    x[__all_columns_unclassified.index(string_definition['city'])] = 1
+    x[__all_columns_unclassified.index(string_definition['home_type'])] = 1
+    x[__all_columns_unclassified.index(string_definition['is_smoking'])] = 1
+
+    x[0] = quantity_definition['beds']
+    x[1] = quantity_definition['baths']
+    x[2] = quantity_definition['sq_feet']
+    x[3] = quantity_definition['cats']
+    x[4] = quantity_definition['dogs']
+    x[5] = quantity_definition['nbr_beds']
+
+    prediction = __model.predict(np.array([x]))
+    return round(float(prediction[0][0]), 2)
+
+if __name__ == "__main__":
+    dummies = {'province':'alberta', 'city':'airdrie', 'home_type':'townhouse', 'is_smoking':'non-smoking'}
+    quantities = {'beds':2, 'baths':2.5, 'sq_feet':1403.0,'cats':1, 'dogs':1,'nbr_beds':2.0}
+    print(get_estimation_price(dummies, quantities))
